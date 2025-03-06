d0b57170740a2991dc7429f5cb36af88b09ded51
yoyokino <1359880421@qq.com>
2025-03-06 16:05:11 +0800

    0306 Delete old_version data_model

--- models/old_version/entity.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class SystemEntity(BaseModel):
-    name: str
-    type: str  # Actor/Operation/DataStore等
-    attributes: dict
-    relations: List[str]
